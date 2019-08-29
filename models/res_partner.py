1310e38ddda2bd7f3a7768b95a5ee6edd94c5efc
manuelcalerosolis <manuelcalerosolis@gmail.com>
2019-08-29 13:22:46 +0200

    initial commit

---
+++ models/res_partner.py
@@ -0,0 +1,11 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    commercial_name = fields.Char(string='Commercial Name')
+
+    admin_comment = fields.Text(string='Administrator Notes')
